2a1085efdda8a632501e7530a3804b83c36cc830
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-03-08 18:02:42 +0900

    업뎃

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          K-digital 6기 김우정
-        </p>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            K-digital 6기 김우정
+          </p>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+      <div>402호</div>
+    </>
   );
 }
 
