47b055b5388987e528b3de02fe016d3e04cd998c
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-03-12 12:59:58 +0900

    jsx 문법 추가

--- src/01/Hello.js
+++ src/01/Hello.js
@@ -1,6 +1,6 @@
 import "./Hello.css";
 function Hello(){
-    let n= Math.floor(Math.random()*100)+1;
+    let n= Math.floor(Math.random()*99)+1;
     const st1={
         backgroundColor:"darkgreen",
         color:"white"
@@ -8,12 +8,24 @@ function Hello(){
     // let s='';
     // if(n%2==0)s="짝수";
     // else s="홀수";
+    let x;
+    const currentTime=new Date();
 
     return(
         <h1>
             <span style={st1}>Hello</span>
-            <span style={{display:"inline-flex", margin:"10px"}}>{n}</span>
-            {n%2===0 ? "짝수":"홀수"}
+            <span style={{display:"inline-flex", margin:"10px"}}>
+                {n<10 ? `0${n}`:n}
+                {/* {n} */}
+                </span>
+            {n%2===0 ? <span style={{color:"red"}}>짝수</span>:<span style={{color:"green"}}>홀수</span>}
+            {n%2===0 &&"🤢"}
+            {n%2===1 && "👍"}
+            {/* <p>{x||" x는 undifined입니다."}</p> */}
+            {/* 에러를 막기 위해서 미리 써 둠 */}
+            <p>
+                {currentTime.toLocaleTimeString()}
+            </p>
         </h1>
     );
 }
