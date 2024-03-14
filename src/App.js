28787649e73ed00fd504498683b869a4de0857a9
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-03-14 12:16:55 +0900

    테일윈드 적용

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ import HelloCss from './02/HelloCss';
 function App() {
   return (
     <>
-      <div className="App">
+      <div className="flex flex-col 
+                      w-full max-w-screen-xl
+                      h-screen
+                      mx-auto
+                      overscroll-y-auto">
         {/* <MainHeader /> */}
         {/* <MainHeader />
         <MainHeader /> */}
@@ -16,7 +20,25 @@ function App() {
       <Hello />
       <Hello /> */}
       {/* <MyClock /> */}
-      <HelloCss />
+      {/* <HelloCss /> */}
+        <header className="flex justify-between items-center
+                          h-20
+                          p-10
+                          text-xl font-bold text-white
+                          bg-[#6ABEA7]">
+          <div>리액트 실습</div>
+          <div>홈</div>
+          </header>
+        <main className="grow">
+          <HelloCss />
+          </main>
+        <footer className="flex justify-center items-center
+                          h-20
+                          p-30
+                          text-base font-mono text-[#BDFFFD]
+                          bg-[#5E6973]">
+          ⓒ 2024 kimujeong. All right reserved.
+          </footer>
       </div>
 
     </>
