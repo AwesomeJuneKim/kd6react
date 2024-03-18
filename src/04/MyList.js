339570421ef8e599ad192c1ab617a5b631f1e957
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-03-18 14:54:33 +0900

    이벤트를 붙여서 콘솔에 표시

--- src/04/MyList.js
+++ src/04/MyList.js
@@ -1,6 +1,12 @@
 import { FaHeart } from "react-icons/fa";
 
 export default function MyList({title, imgUrl, content}) {
+  let cnt=0;
+  const handleLike=(t)=>{
+    console.log('handleLike'+t)
+    cnt=cnt+1;
+    console.log(`cnt=${cnt}`)
+  }
   return (
     <div className="flex flex-row border-2 rounded border-gray-500
                     w-full">
@@ -8,12 +14,18 @@ export default function MyList({title, imgUrl, content}) {
         <img src={imgUrl} alt={title} />
       </div>
       <div className="w-3/4 h-auto">
-        <h1 className="font-bold pl-3">{title}</h1>
-        <p className="pl-3">{content}</p>
+        <div className="h-4/6">
+          <h1 className="font-bold pl-3">{title}</h1>
+          <p className="pl-3">{content}</p>
         </div>
-      <div className="w-full flex justify-end mt-5 pr-3
-                    text-red-700 items-baseline"><FaHeart/>
-                    <span className="text-black font-bold">좋아요</span></div>
+        
+        <div className="w-full flex justify-end mt-5 pr-3 h-2/6
+                        items-baseline" onClick={()=>{handleLike(title)}}>
+                        <FaHeart className=" text-red-700 hover:text-black"/>
+                        <span className="font-bold">좋아요</span>
+                        <span>{cnt}</span>
+        </div>
+      </div>
     </div>
   )
 }
