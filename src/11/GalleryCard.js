d764cd120ed4d43def17073c46672ddd9cc64d88
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-04-02 09:56:09 +0900

    갤러리카드 수정완료

--- src/11/GalleryCard.js
+++ src/11/GalleryCard.js
@@ -1,7 +1,6 @@
 
-export default function GalleryCard({imgUrl,title,ptitle,ktag,content}) {
-    if(ktag===undefined) ktag=''
-    else if (ktag.includes(',')) {
+export default function GalleryCard({imgUrl,title,ptitle,ktag}) {
+    if (ktag.includes(',')) {
       ktag = ktag.split(',').map(item =>
         <span key={item}
           className="inline-block bg-gray-200 
@@ -35,10 +34,9 @@ export default function GalleryCard({imgUrl,title,ptitle,ktag,content}) {
           {ptitle}
         </p>
       </div>
-      {ktag !='' && <div className="px-6 pt-4 pb-2">
+      <div className="px-6 pt-4 pb-2">
         {ktag}
-      </div>}
-      <div className="m-2 p-2 bg-indigo-200 text-white rounded-lg">{content}</div>
+      </div>
     </div>
   )
   }
