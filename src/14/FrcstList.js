1d0b1705ea568a740a929680c038b1833a2db143
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-04-05 16:52:49 +0900

    일기예보 완료

--- src/14/FrcstList.js
+++ src/14/FrcstList.js
@@ -12,16 +12,24 @@ export default function FrcstList() {
     // console.log(dt,area,x,y,gubun);
 
     const[selitem,setSelitem]=useState()
-    const [selitemNm,setSelitemNm] =useState()
+    // const [selitemNm,setSelitemNm] =useState()
     const [vilage,setVilage]=useState([])
     const [vilcard, setVilcard]=useState([]);
     const itemRef=useRef()
     const ops= getcode.filter(item=>item["예보구분"]===gubun)
                         .map(item=>`${item["항목명"]}-${item["항목값"]}`)
     useEffect(()=>{
-    let url=`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?`
-    url=`${url}serviceKey=${process.env.REACT_APP_APIKEY}`
-    url=`${url}&pageNo=1&numOfRows=1000&dataType=json&base_date=${dt}&base_time=0500&nx=${x}&ny=${y}`
+    let url='';
+    if(gubun==='초단기예보'){
+        url=`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?`
+        url=`${url}serviceKey=${process.env.REACT_APP_APIKEY}`
+        url=`${url}&pageNo=1&numOfRows=1000&dataType=json&base_date=${dt}&base_time=0630&nx=${x}&ny=${y}`
+    } else{
+        url=`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?`
+        url=`${url}serviceKey=${process.env.REACT_APP_APIKEY}`
+        url=`${url}&pageNo=1&numOfRows=1000&dataType=json&base_date=${dt}&base_time=0500&nx=${x}&ny=${y}`
+        
+    }
     console.log(url)
 
     fetch(url)
@@ -36,17 +44,18 @@ export default function FrcstList() {
       return;
   }
   console.log(itemRef.current.value)
-  setSelitemNm(itemRef.current.value.split('-')[0])
+//   setSelitemNm(itemRef.current.value.split('-')[0])
   setSelitem(itemRef.current.value.split('-')[1])
   }
   useEffect(()=>{
     if (!selitem) return;
     const tm=vilage.filter(item=>item.category===selitem)
         .map(item=>
-        <tr className="hover:bg-slate-200" key={item.baseTime+item.fcstTime}>
+        <tr className="hover:bg-slate-200" key={item.fcstDate+item.fcstTime}>
             <td>{`${item.baseDate.substring(0,4)}-${item.baseDate.substring(4,6)}-${item.baseDate.substring(6,8)}`}</td>
             <td>{`${item.baseTime.substr(0,2)}:${item.baseTime.substr(2,4)}`}</td>
             <td>{item.category}</td>
+            <td>{`${item.fcstDate.substring(0,4)}-${item.fcstDate.substring(4,6)}-${item.fcstDate.substring(6,8)}`}</td>
             <td>{`${item.fcstTime.substr(0,2)}:${item.fcstTime.substr(2,4)}`}</td>
             <td>{item.category==="T1H"? `${item.fcstValue}℃`:item.fcstValue}</td>
         </tr>
@@ -57,8 +66,8 @@ setVilcard(tm);
   return (
 
       <div className="w-11/12 h-full flex flex-col justify-start items-center shadow-md sm:rounded-lg">
-        <div className="flex">
-            <div className="w-2/3 font-extrabold text-lg m-4 text-gray-700">{`${dt.substring(0,4)}-${dt.substring(4,6)}-${dt.substring(6,8)} ${area}의 ${gubun}`}</div>
+        <div className="flex w-full">
+            <div className="w-4/5 font-extrabold text-lg m-4 text-gray-700">{`${dt.substring(0,4)}-${dt.substring(4,6)}-${dt.substring(6,8)} ${area}의 ${gubun}`}</div>
             <TailSelect ops={ops}
                         selRef={itemRef}
                         handleLocation={handleItem}
@@ -67,9 +76,10 @@ setVilcard(tm);
         <table className="w-full text-gray-500 text-sm text-center">
             <thead>
                 <tr>
-                    <th>예측날짜</th>
+                    <th>관측날짜</th>
                     <th>예보시간</th>
                     <th>기상상태</th>
+                    <th>예측날짜</th>
                     <th>예측시간</th>
                     <th>예측값</th>
                 </tr>
