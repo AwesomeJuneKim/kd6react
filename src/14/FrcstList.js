b236c2e67543335d88783261b6b774a9f101ab0c
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-04-05 15:49:39 +0900

    쿼리스트링으로  전달

--- src/14/FrcstList.js
+++ src/14/FrcstList.js
@@ -1,6 +1,12 @@
-import React from 'react'
-
+import { useSearchParams } from "react-router-dom"
 export default function FrcstList() {
+    const[queryParams]=useSearchParams();
+    const dt=queryParams.get('dt');
+    const area=queryParams.get('area');
+    const x=queryParams.get('x');
+    const y=queryParams.get('y');
+    const gubun=queryParams.get('gubun');
+    console.log(dt,area,x,y,gubun);
   return (
     <div>
       
